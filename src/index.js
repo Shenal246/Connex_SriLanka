b7b9859915ac0dde48c19da567813bcfd771fb15
Shenal Perera <pererashenal369@gmail.com>
2024-05-15 15:38:39 +0530

    Aos installed

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'aos/dist/aos.css'; // Import AOS CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
