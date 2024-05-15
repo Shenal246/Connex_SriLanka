f45dc33565af951b1fb036c996c1190c60155845
Shenal Perera <pererashenal369@gmail.com>
2024-05-15 15:28:23 +0530

    Revert "Vendors Updated"
    This reverts commit 420ff3d60dbe01ff54e8058f44b3a1a08d882ef0.

--- src/Components/Home/Vendors/Vendors.js
+++ src/Components/Home/Vendors/Vendors.js
@@ -24,58 +24,9 @@ import ven11 from '../../../images/vendorLogos/11.png';
 import ven12 from '../../../images/vendorLogos/12.png';
 import ven13 from '../../../images/vendorLogos/13.webp';
 import ven14 from '../../../images/vendorLogos/14.png';
-import ven15 from '../../../images/vendorLogos/15.png';
-import ven16 from '../../../images/vendorLogos/16.png';
-import ven17 from '../../../images/vendorLogos/17.png';
-import ven18 from '../../../images/vendorLogos/18.png';
-import ven19 from '../../../images/vendorLogos/19.png';
-import ven20 from '../../../images/vendorLogos/20.png';
-import ven21 from '../../../images/vendorLogos/21.png';
-import ven22 from '../../../images/vendorLogos/22.png';
-import ven23 from '../../../images/vendorLogos/23.png';
-import ven24 from '../../../images/vendorLogos/24.png';
-import ven25 from '../../../images/vendorLogos/25.png';
-import ven26 from '../../../images/vendorLogos/26.png';
-import ven27 from '../../../images/vendorLogos/27.png';
-import ven28 from '../../../images/vendorLogos/28.png';
-import ven29 from '../../../images/vendorLogos/29.png';
-import ven30 from '../../../images/vendorLogos/30.png';
-import ven31 from '../../../images/vendorLogos/31.png';
-import ven32 from '../../../images/vendorLogos/32.png';
-import ven33 from '../../../images/vendorLogos/33.png';
-import ven34 from '../../../images/vendorLogos/34.png';
-import ven35 from '../../../images/vendorLogos/35.png';
-import ven36 from '../../../images/vendorLogos/36.png';
-import ven37 from '../../../images/vendorLogos/37.png';
-import ven38 from '../../../images/vendorLogos/38.png';
-import ven39 from '../../../images/vendorLogos/39.png';
-import ven40 from '../../../images/vendorLogos/40.png';
-import ven41 from '../../../images/vendorLogos/41.png';
-import ven42 from '../../../images/vendorLogos/42.png';
-import ven43 from '../../../images/vendorLogos/43.png';
-import ven44 from '../../../images/vendorLogos/44.png';
-import ven45 from '../../../images/vendorLogos/45.png';
-import ven46 from '../../../images/vendorLogos/46.png';
-import ven47 from '../../../images/vendorLogos/47.png';
-import ven48 from '../../../images/vendorLogos/48.png';
-import ven49 from '../../../images/vendorLogos/49.png';
-import ven50 from '../../../images/vendorLogos/50.png';
-import ven51 from '../../../images/vendorLogos/51.png';
-import ven52 from '../../../images/vendorLogos/52.png';
-import ven53 from '../../../images/vendorLogos/53.png';
-import ven54 from '../../../images/vendorLogos/54.png';
-import ven55 from '../../../images/vendorLogos/55.png';
-import ven56 from '../../../images/vendorLogos/56.png';
-import ven57 from '../../../images/vendorLogos/57.png';
 // Add import statements for all your images here...
 
-const imagePaths = [ven1, ven2, ven3, ven4, ven5, ven6, ven7, ven8, ven9, ven10,
-  ven11, ven12, ven13, ven14, ven15, ven16, ven17, ven18, ven19, ven20,
-  ven21, ven22, ven23, ven24, ven25, ven26, ven27, ven28, ven29, ven30,
-  ven31, ven32, ven33, ven34, ven35, ven36, ven37, ven38, ven39, ven40,
-  ven41, ven42, ven43, ven44, ven45, ven46, ven47, ven48, ven49, ven50,
-  ven51, ven52, ven53, ven54, ven55, ven56, ven57
-]; // Add all your image paths to this array
+const imagePaths = [ven1, ven2, ven3, ven4, ven5, ven6, ven7, ven8, ven9, ven10, ven11, ven12, ven13, ven14]; // Add all your image paths to this array
 
 const Vendors = () => {
   return (
