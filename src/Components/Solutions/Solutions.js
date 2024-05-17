d09b36a57cd59223cd3da811a54691b25f1bf341
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-05-17 15:19:09 +0530

    css update

--- src/Components/Solutions/Solutions.js
+++ src/Components/Solutions/Solutions.js
@@ -10,81 +10,80 @@ import servers from '../../images/pillers/severBackup.jpg';
 
 function Solutions() {
   return (
-    <section id='hero ' className='hero1' >
+    <section id='hero' className='hero1'>
       <div className="container text-center">
-      <div className='row'>
-                <div className="row text">
-                    <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
-                    <div className="col-4" data-aos="fade-up" data-aos-delay="100"><p id='topic'>Solutions</p></div>
-                    <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
-                </div>
-            </div>
+        <div className='row'>
+          <div className="row text">
+            <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
+            <div className="col-4" data-aos="fade-up" data-aos-delay="100"><p id='topic'>Solutions</p></div>
+            <div className="col-4" data-aos="fade-up" data-aos-delay="100"><hr /></div>
+          </div>
+        </div>
         <div className="row row-cols-2 row-cols-lg-4 g-2 g-lg-3 rowmargin">
           <div className="col">
-            <div className="card crd" >
-              <img src={Perimie} className="card-img-top" alt="..." />
+            <div className="card crd">
+              <img src={Perimie} className="card-img-top" alt="Perimeter and Internal Security" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Perimeter and Internal Security</p>
               </div>
             </div>
           </div>
-          <div className="col ">
-            <div className="card crd" >
-              <img src={cyber} className="card-img-top" alt="..." />
+          <div className="col">
+            <div className="card crd">
+              <img src={cyber} className="card-img-top" alt="Cyber Security Governance & Compliance" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Cyber Security Governance & Compliance</p>
               </div>
             </div>
           </div>
-          <div className="col ">
-            <div className="card crd" >
-              <img src={Authitianit} className="card-img-top" alt="..." />
+          <div className="col">
+            <div className="card crd">
+              <img src={Authitianit} className="card-img-top" alt="Authentication & Identity Management" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Authentication & Identity Management</p>
               </div>
             </div>
           </div>
-          <div className="col ">
-            <div className="card crd" >
-              <img src={Security} className="card-img-top" alt="..." />
+          <div className="col">
+            <div className="card crd">
+              <img src={Security} className="card-img-top" alt="Security Management" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Security Management</p>
               </div>
             </div>
           </div>
-          <div className="col ">
-            <div className="card crd" >
-              <img src={datapoint} className="card-img-top" alt="..." />
+          <div className="col">
+            <div className="card crd">
+              <img src={datapoint} className="card-img-top" alt="Endpoint Security" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Endpoint Security</p>
               </div>
             </div>
           </div>
-          <div className="col ">
-            <div className="card crd" >
-              <img src={networking} className="card-img-top" alt="..." />
+          <div className="col">
+            <div className="card crd">
+              <img src={networking} className="card-img-top" alt="Networking" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Networking</p>
               </div>
             </div>
           </div>
-          <div className="col ">
-            <div className="card crd" >
-              <img src={endpoint} className="card-img-top" alt="..." />
+          <div className="col">
+            <div className="card crd">
+              <img src={endpoint} className="card-img-top" alt="Data Center Infrastructure and Infrastructure Monitoring" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Data Center Infrastructure and Infrastructure Monitoring</p>
               </div>
             </div>
           </div>
           <div className="col column">
-            <div className="card crd" >
-              <img src={servers} className="card-img-top" alt="..." />
+            <div className="card crd">
+              <img src={servers} className="card-img-top" alt="Server Storage & Backup Solutions" />
               <div className="card-body cardSize">
                 <p className="card-text txts">Server Storage & Backup Solutions</p>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </section>
