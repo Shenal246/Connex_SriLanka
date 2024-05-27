3f5f2a2af8a9bd493c96bc50bcf38d7b34ef8d5c
Shenal Perera <pererashenal369@gmail.com>
2024-05-27 15:00:18 +0530

    ContactUs and topbar

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -7,9 +7,9 @@ const Topbar = () => {
             <div className="container d-flex justify-content-center justify-content-md-between">
                 <div className="contact-info d-flex align-items-center">
                     <i className="bi bi-envelope d-flex align-items-center">
-                        <a href="mailto:shenalperera246@gmail.com">sampleemail@gmail.com</a>
+                        <a href="mailto:info@connexit.biz">info@connexit.biz</a>
                     </i>
-                    <i className="bi bi-phone d-flex align-items-center ms-4"><span>011 2254877</span></i>
+                    <i className="bi bi-phone d-flex align-items-center ms-4"><span>+94 114 693 355</span></i>
                 </div>
                 <div className="social-links d-flex d-md-flex align-items-center">
                     <a href="#" target="_blank" className="twitter"><i className="fa-brands fa-x-twitter twitter"></i></a>
