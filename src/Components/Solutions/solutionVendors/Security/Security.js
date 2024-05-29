634d4b4ccb7a139569971342de813f909ae7f274
Shenal Perera <pererashenal369@gmail.com>
2024-05-29 12:01:16 +0530

    Database loaded

--- src/Components/Solutions/solutionVendors/Security/Security.js
+++ src/Components/Solutions/solutionVendors/Security/Security.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import './Security.css';
 import ven1 from '../../../../images/vendorLogos/1.png';
-import ven2 from '../../../../images/vendorLogos/10.png';
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { Row } from 'react-bootstrap';
 
 
 function Security() {
+    const [vendors, setVendors] = useState([]);
+    const [show, setShow] = useState(false);
+    const [currentVendor, setCurrentVendor] = useState(null);
+
+    useEffect(() => {
+        const values = {
+            query: "SELECT v.name,v.vlink,v.description,v.status,v.img FROM vendor v JOIN vendorpillor vp ON v.id = vp.vendor_id WHERE v.status=1 AND vp.pillor_id=4;",
+            key: "Cr6re8VRBm"
+        };
+
+        axios.post("http://192.168.13.75:5000/search", values).then((response) => {
+            setVendors(response.data);
+        }).catch((err) => {
+            console.log(err);
+        });
+
+        console.log(vendors);
+
+    }, []);
+
+    const handleCardClick = (vend) => {
+        setCurrentVendor(vend);
+    };
+
     return (
         <section>
             <div className="container">
@@ -16,28 +44,46 @@ function Security() {
                     </div>
                 </div>
                 <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 rowmargin">
-                    <div className="col">
+                    {vendors && vendors.map((vend, index) => (
+                        <div className="col" key={index}>
 
-                        <div class="card h-100">
-                            <img src={ven1} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title vendorTitel">Card title</h5>
+                            <div class="card h-100" onClick={() => { setShow(true); handleCardClick(vend); }}>
+                                <img src={ven1} class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title vendorTitel">{vend.name}</h5>
+                                </div>
                             </div>
+
                         </div>
 
-                    </div>
-                    <div className="col">
+                    ))}
 
-                        <div class="card h-100">
-                            <img src={ven2} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title vendorTitel">Card title</h5>
-                            </div>
-                        </div>
-
-                    </div>
-                   
                 </div>
+                <Modal
+                    show={show}
+                    onHide={() => setShow(false)}
+                    aria-labelledby="example-custom-modal-styling-title"
+                    size='xl'
+
+                >
+                    <Modal.Header closeButton closeVariant='white' className='modelHeader'>
+                        <Modal.Title id="example-custom-modal-styling-title">
+                            {currentVendor && currentVendor.name}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body scrollable className='btnBody'>
+                        <Row>
+                            <p>
+                                {currentVendor && currentVendor.description}
+                            </p>
+
+                        </Row>
+                        <Row>
+                            <a href={currentVendor && currentVendor.vlink} target='_blank'><Button variant="primary" className='websiteButton'>Visit Website</Button></a>
+                        </Row>
+
+                    </Modal.Body>
+                </Modal>
             </div>
         </section>
     );
