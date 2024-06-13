3bc137c7b41b6e1e5d36daa4dc123d0ca1b24af6
RivinduTharaka <rivindu.20220728@iit.ac.lk>
2024-06-13 08:40:34 +0530

    portal

--- src/Components/Portal/Portal.js
+++ src/Components/Portal/Portal.js
@@ -92,7 +92,7 @@ const SingleVendor = () => {
 
                         <div className='productNames'>
                             
-                            
+                           
                         </div>
                         
                     </div>
