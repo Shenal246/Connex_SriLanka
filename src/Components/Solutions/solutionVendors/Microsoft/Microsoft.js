9ad34f5b612917748cf44ffcb414cda76695ab2e
Shenal Perera <pererashenal369@gmail.com>
2024-06-24 14:36:53 +0530

    Microsoft created

--- src/Components/Solutions/solutionVendors/Microsoft/Microsoft.js
+++ src/Components/Solutions/solutionVendors/Microsoft/Microsoft.js
@@ -1,106 +1,101 @@
-import React from 'react';
+import React from 'react'
+import micimg from '../../../../images/vendorLogos/2.png'
 import './Microsoft.css';
-import ven1 from '../../../../images/vendorLogos/1.png';
-import { useState, useEffect } from 'react';
-import axios from "axios";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import { Row } from 'react-bootstrap';
-import connections from '../../../../config';
-import { useNavigate } from 'react-router-dom';
 
 function Microsoft() {
-    const [vendors, setVendors] = useState([]);
-    const [show, setShow] = useState(false);
-    const [currentVendor, setCurrentVendor] = useState(null);
-    const navigate = useNavigate();
-    const serverlink = connections.serverLink;
-
-    useEffect(() => {
-        const values = {
-            query: "SELECT name,des,wlink,status,image_data,cnt,Server_Storage_Backup_Solutions FROM Vendor WHERE Server_Storage_Backup_Solutions='true' AND status=1 AND cnt=1;",
-            key: "Cr6re8VRBm"
-        };
-
-        axios.post(serverlink, values).then((response) => {
-            setVendors(response.data);
-        }).catch((err) => {
-            console.log(err);
-        });
-
-        console.log(vendors);
-
-    }, []);
-
-    const handleCardClick = (vend) => {
-        navigate(`/SL/Solutions/ServerBackup/${vend.name}`, { state: { vend } });
-    };
     return (
-        <section className='heroSolution'>
-            <div className="container">
-                <div className='row '>
-                    <div className="row text headingrow">
-                        <div className="col-3" data-aos="fade-up" data-aos-delay="100"><hr /></div>
-                        <div className="col-6" data-aos="fade-up" data-aos-delay="100"><p id='topic'>Server Storage & Backup Solutions</p></div>
-                        <div className="col-3" data-aos="fade-up" data-aos-delay="100"><hr /></div>
+        <div className='section'>
+            <div className='container'>
+                <div className='row firstrow'>
+                    <div className='col-lg-3'>
+                        <img src={micimg} className='miclogo' />
+                    </div>
+
+                    <div className='col-lg'>
+                        <p className='AuthText'>Cloud Solution Provider </p>
+                    </div>
+
+                    <div className='col'></div>
+                </div>
+                <div className='row descontainer'>
+                    {/* Column one */}
+                    <div className='col'>
+                        <h6 className='micsectopic'>Microsoft Security</h6>
+                        <dl className='unlisted'>
+                            <dt>Security AI</dt>
+                            <dd>- Microsoft Security Copilot</dd>
+                        </dl>
+
+                        <dl className='unlisted'>
+                            <dt>Identity & Access</dt>
+                            <dd>- Azure Active Directory part of Microsoft Entra </dd>
+                            <dd>- Microsoft Entra Identity Governance </dd>
+                            <dd>- Microsoft Entra Permissions Management </dd>
+                            <dd>- Microsoft Entra Verified ID </dd>
+                            <dd>- Microsoft Entra Workload Identities </dd>
+                            <dd>- Azure Key Vault  </dd>
+                        </dl>
+
+                        <dl className='unlisted'>
+                            <dt>SIEM & XDR</dt>
+                            <dd>- Microsoft Sentinel </dd>
+                            <dd>- Microsoft Defender for Cloud </dd>
+                            <dd>- Microsoft 365 Defender </dd>
+                            <dd>- Microsoft Defender for Endpoint </dd>
+                            <dd>- Microsoft Defender for Office 365 </dd>
+                            <dd>- Microsoft Defender for Identity </dd>
+                            <dd>- Microsoft Defender for Cloud Apps </dd>
+                            <dd>- Microsoft Defender Vulnerability Management </dd>
+                            <dd>- Microsoft Defender Threat Intelligence </dd>
+                        </dl>
+                    </div>
+
+                    {/* Column two */}
+                    <div className='col'>
+                        <h6 className='micsectopic'>Microsoft Security</h6>
+
+                        <dl className='unlisted'>
+                            <dt>Cloud Security</dt>
+                            <dd>- Microsoft Defender for Cloud  </dd>
+                            <dd>- Microsoft Defender Cloud Security Posture </dd>
+                            <dd>- Microsoft Defender for DevOps </dd>
+                            <dd>- Microsoft Defender External Attack </dd>
+                            <dd>- Surface Management </dd>
+                            <dd>- Azure Firewall </dd>
+                            <dd>- Azure Web App Firewall </dd>
+                            <dd>- Azure DDoS Protection </dd>
+                            <dd>- GitHub Advanced Security </dd>
+                        </dl>
+
+                        <dl className='unlisted'>
+                            <dt>Endpoint Security & Management</dt>
+                            <dd>- Microsoft Defender for Endpoint  </dd>
+                            <dd>- Microsoft 365 Defender  </dd>
+                            <dd>- Microsoft Intune core capabilities  </dd>
+                            <dd>- Microsoft Intune Endpoint Privilege Management </dd>
+                            <dd>- Microsoft Intune Remote Help </dd>
+                            <dd>- Microsoft Defender for IoT </dd>
+                            <dd>- Microsoft Defender for Business  </dd>
+                            <dd>- Microsoft Defender Vulnerability Management </dd>
+                        </dl>
+                    </div>
+
+                    {/* Column three */}
+                    <div className='col'>
+                        <h6 className='micsectopic3'>Microsoft Modern Workplace</h6>
+
+                        <dl className='unlisted'>
+                            <dd>- Microsoft 365 </dd>
+                            <dd>- Office 365 </dd>
+                            <dd>- Windows 10 Enterprise </dd>
+                            <dd>- Enterprise Mobility + Security (EMS) </dd>
+                            <dd>- Business Process Automationt </dd>
+                        </dl>
                     </div>
                 </div>
-                <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 rowmargin">
-
-                    {vendors && vendors.map((vend, index) => (
-                        <div className="col" key={index}>
-
-                            <div class="card h-100" onClick={() => {  handleCardClick(vend); }}>
-                                {vend.image_data ? (
-                                    <>
-                                        <img
-                                            src={`data:image/jpeg;base64,${vend.image_data}`}
-                                            alt={vend.title}
-                                            className="card-img-top"
-
-                                        />
-                                    </>
-                                ) : (
-                                    <p>No Image Available</p>
-                                )}
-                                <div class="card-body">
-                                    <h5 class="card-title vendorTitel">{vend.name}</h5>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    ))}
-                </div>
-
-                <Modal
-                    show={show}
-                    onHide={() => setShow(false)}
-                    aria-labelledby="example-custom-modal-styling-title"
-                    size='xl'
-
-                >
-                    <Modal.Header closeButton closeVariant='white' className='modelHeader'>
-                        <Modal.Title id="example-custom-modal-styling-title">
-                            {currentVendor && currentVendor.name}
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body scrollable className='btnBody'>
-                        <Row>
-                            <p className='pdes'>
-                                {currentVendor && currentVendor.des}
-                            </p>
-
-                        </Row>
-                        <Row>
-                            <a href={currentVendor && currentVendor.wlink} target='_blank'><Button variant="primary" className='websiteButton'>Visit Website</Button></a>
-                        </Row>
-
-                    </Modal.Body>
-                </Modal>
             </div>
-        </section>
-    );
+        </div>
+    )
 }
 
-export default Microsoft;
+export default Microsoft
