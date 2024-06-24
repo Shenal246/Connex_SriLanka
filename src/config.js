cf64809a381393638fc4f7f90243b37a2883bc16
Shenal Perera <pererashenal369@gmail.com>
2024-06-24 16:51:24 +0530

    Update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    serverLink:'http://192.168.13.75:3001/search',
-    serverLinkInsert:'http://192.168.13.75:3001/insert'
+    serverLink: 'http://api.connexlk.biz:3001/search',
+    serverLinkInsert: 'http://api.connexlk.biz:3001/insert'
 };
 
 export default config;
